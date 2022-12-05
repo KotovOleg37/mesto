d54b58fdc3b7984985e81890536e1befb65707b5
Oleg Kotov <spartakovec38@gmail.com>
2022-12-05 23:37:28 +0500

    fix  #2

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,36 @@
 /*Обьявление переменных*/ 
-const popupButton = document.querySelector('.profile__button');
+const profileButton = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-const save = document.querySelector('.popup__button');
+const popupButtonSave = document.querySelector('.popup__form');
 let popupName = document.querySelector('.popup__name');
 let popupJob = document.querySelector('.popup__job');
 let titleName = document.querySelector('.profile__title-text')
 let titleJob = document.querySelector('.profile__subtitle')
 
-//Открытие-закрытие popap*
-popupButton.addEventListener('click', function (){
+
+//Открытие popap. При открытии формы поля «Имя» и «О себе» должны быть заполнены теми значениями, которые отображаются на странице."
+function openPopup(){
   popup.classList.add('popup_opened');
-});
+  popupName.value = titleName.textContent;
+  popupJob.value = titleJob.textContent;
+}
+profileButton.addEventListener('click', openPopup);
 
-popupClose.addEventListener('click', function (){
+//Закрытие popap
+function closePopup(){
   popup.classList.remove('popup_opened');
-});
+}
+popupClose.addEventListener('click', closePopup);
 
-
-
-//Редактирование имени и информации о себе
+//Обработчик отправки формы хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault();
   titleName.textContent = popupName.value;
   titleJob.textContent = popupJob.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
-
-save.addEventListener('click', handleFormSubmit); 
+// Прикрепляем обработчик к форме.Он будет следить за событием “submit” - «отправка»
+popupButtonSave.addEventListener('submit', handleFormSubmit); 
 
 
